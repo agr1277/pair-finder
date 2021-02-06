d845c84899c7f271095176a120c948e3e2da1324
Chris Perry <chris@cwperry.dev>
2021-02-06 18:39:51 -0500

    add combinatorics library

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A simple utility to play with strings"
   :license {:name "MIT License"
             :url  "https://spdx.org/licenses/MIT.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :main ^:skip-aot pair-finder.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot      :all

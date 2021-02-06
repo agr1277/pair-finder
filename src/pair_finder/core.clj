1e309bdb4dc641d85fe544a8e85ba9752a73080a
Chris Perry <chris@cwperry.dev>
2021-02-06 18:46:58 -0500

    count element pair occurrences, using combinations and frequencies

--- src/pair_finder/core.clj
+++ src/pair_finder/core.clj
@@ -1,6 +1,7 @@
 (ns pair-finder.core
   (:require [clojure.string :as s]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.math.combinatorics :refer [combinations]])
   (:gen-class))
 
 (defn parse-row [row]
@@ -12,6 +13,13 @@
          line-seq
          (map parse-row)
          (into []))))
+
+(defn count-element-pairs [rows]
+  (->> rows
+       (map #(combinations % 2))
+       (apply concat)
+       (map vec)
+       (frequencies)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/pair_finder/core_test.clj
+++ test/pair_finder/core_test.clj
@@ -11,3 +11,15 @@
     (is (= [["A" "B" "C" "D" "E"]
             ["1" "2" "3" "4"]
             ["Hello" "World"]] (load-rows-from-file "test/resources/test_rows.txt")))))
+
+(deftest test-count-pairs
+  (testing "counts the occurrences of elements seen together in a vector of element vectors"
+    (let [test-rows [["A" "B" "C"]
+                      ["A" "B" "D"]
+                      ["B" "D" "C"]]]
+      (is (= {["A" "B"] 2
+              ["A" "C"] 1
+              ["B" "C"] 2
+              ["A" "D"] 1
+              ["B" "D"] 2
+              ["D" "C"] 1} (count-element-pairs test-rows))))))
